703f0213f773a577217a53637ffbbe75e31ef2d2
Dakson Cruz <daksonc@gmail.com>
2022-11-21 12:46:11 -0300

    feat: cria layout da págian de Tasks

--- src/page/Tasks/index.tsx
+++ src/page/Tasks/index.tsx
@@ -1,4 +1,17 @@
-import { Box, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import { useEffect } from "react";
 
 import { Sidebar } from "../../components/Sidebar";
@@ -19,7 +32,49 @@ export function Tasks() {
         alignItems="center"
         bgGradient="linear(to-t, gray.900, gray.600)"
       >
-        <Text color="gray.500">MyTasks</Text>
+        <Flex direction="column" w="100%" maxW="1120px" mx="auto" px="6">
+          <Text color="gray.500" fontSize="2xl" fontWeight="bold" mb="8">
+            MyTasks
+          </Text>
+          <TableContainer bg="gray.800" borderRadius="8" boxShadow="lg" p="4">
+            <Table variant="striped" colorScheme="blackAlpha">
+              <TableCaption placement="top">
+                Imperial to metric conversion factors
+              </TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>To convert</Th>
+                  <Th>into</Th>
+                  <Th isNumeric>multiply by</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>inches</Td>
+                  <Td>millimetres (mm)</Td>
+                  <Td isNumeric>25.4</Td>
+                </Tr>
+                <Tr>
+                  <Td>feet</Td>
+                  <Td>centimetres (cm)</Td>
+                  <Td isNumeric>30.48</Td>
+                </Tr>
+                <Tr>
+                  <Td>yards</Td>
+                  <Td>metres (m)</Td>
+                  <Td isNumeric>0.91444</Td>
+                </Tr>
+              </Tbody>
+              <Tfoot>
+                <Tr>
+                  <Th>To convert</Th>
+                  <Th>into</Th>
+                  <Th isNumeric>multiply by</Th>
+                </Tr>
+              </Tfoot>
+            </Table>
+          </TableContainer>
+        </Flex>
       </Box>
     </>
   );
