421f063f9b4903e451a731706caaa9308d96ad00
Dakson Cruz <daksonc@gmail.com>
2022-11-23 12:03:23 -0300

    feat: cria layout da tela de Profile

--- src/page/Profile/index.tsx
+++ src/page/Profile/index.tsx
@@ -1,4 +1,14 @@
-import { Box, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Grid,
+  GridItem,
+  Text,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
 import { useEffect } from "react";
 
 import { Sidebar } from "../../components/Sidebar";
@@ -19,7 +29,64 @@ export function Profile() {
         alignItems="center"
         bgGradient="linear(to-t, gray.900, gray.600)"
       >
-        <Text color="gray.500">Profile</Text>
+        <Box
+          w={["100%", "100%", "100%", "30%"]}
+          maxWidth="550px"
+          h={["80%"]}
+          maxHeight="450px"
+          bg="gray.800"
+          borderRadius="lg"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+        >
+          <Box
+            w="100%"
+            h="38%"
+            backgroundColor="blue.900"
+            borderTopEndRadius="lg"
+            borderTopStartRadius="lg"
+          />
+          <Wrap
+            w="100%"
+            h="100%"
+            mt="-150px"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            spacing="20px"
+            direction={["column"]}
+          >
+            <WrapItem justifyContent="center">
+              <Avatar
+                size="4xl"
+                name="Dani Abrahmov"
+                src="https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+              />
+            </WrapItem>
+            <WrapItem justifyContent="center">
+              <Flex direction="column" alignItems="center">
+                <Text
+                  fontSize="2xl"
+                  fontWeight="bold"
+                  color="gray.100"
+                  textAlign="center"
+                >
+                  Dani Abrahmov
+                </Text>
+                <Text fontSize="md" color="gray.300" textAlign="center">
+                  Front-end Developer
+                </Text>
+              </Flex>
+            </WrapItem>
+            <WrapItem w="100%" justifyContent="center" m={2}>
+              <Button colorScheme="facebook" mb={4}>
+                Edit Profile
+              </Button>
+            </WrapItem>
+          </Wrap>
+        </Box>
       </Box>
     </>
   );
