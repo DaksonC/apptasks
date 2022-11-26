ae19bef25eb7ee9833a33b8fc127eb58d18d11bd
Dakson Cruz <daksonc@gmail.com>
2022-11-25 22:23:20 -0300

    feat: cria loading nas páginas Profile e Tasks

--- src/page/Profile/index.tsx
+++ src/page/Profile/index.tsx
@@ -14,21 +14,25 @@ import {
   ModalHeader,
   ModalOverlay,
   Select,
+  Spinner,
   Text,
   useDisclosure,
   Wrap,
   WrapItem,
 } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 
 export function Profile() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     document.title = "AppTasks ✔️️ |  Profile";
+    setIsLoading(false);
   }, []);
 
   return (
@@ -42,136 +46,140 @@ export function Profile() {
         alignItems="center"
         bgGradient="linear(to-t, gray.900, gray.600)"
       >
-        <Box
-          w={["100%", "100%", "100%", "30%"]}
-          maxWidth="550px"
-          h={["80%"]}
-          maxHeight="450px"
-          bg="gray.800"
-          borderRadius="lg"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          flexDirection="column"
-        >
+        {isLoading ? (
+          <Spinner size="xl" speed="0.7s" />
+        ) : (
           <Box
-            w="100%"
-            h="38%"
-            backgroundColor="blue.900"
-            borderTopEndRadius="lg"
-            borderTopStartRadius="lg"
-          />
-          <Wrap
-            w="100%"
-            h="100%"
-            mt="-150px"
+            w={["100%", "100%", "100%", "30%"]}
+            maxWidth="550px"
+            h={["80%"]}
+            maxHeight="450px"
+            bg="gray.800"
+            borderRadius="lg"
             display="flex"
             justifyContent="center"
             alignItems="center"
-            spacing="20px"
-            direction={["column"]}
+            flexDirection="column"
           >
-            <WrapItem justifyContent="center">
-              <Avatar
-                size="4xl"
-                name="Dani Abrahmov"
-                src="https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-              />
-            </WrapItem>
-            <WrapItem justifyContent="center">
-              <Flex direction="column" alignItems="center">
-                <Text
-                  fontSize="2xl"
-                  fontWeight="bold"
-                  color="gray.100"
-                  textAlign="center"
-                >
-                  Dani Abrahmov
-                </Text>
-                <Text fontSize="md" color="gray.300" textAlign="center">
-                  Front-end Developer
-                </Text>
-              </Flex>
-            </WrapItem>
-            <WrapItem w="100%" justifyContent="center" m={2}>
-              <Button colorScheme="facebook" mb={4} onClick={onOpen}>
-                Edit Profile
-              </Button>
-              <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                  <ModalHeader color="gray.700">
-                    <Box
-                      w="100%"
-                      h="100%"
-                      display="flex"
-                      justifyContent="center"
-                      alignItems="center"
-                      flexDirection="column"
-                    >
-                      <Text fontSize="4xl" color="gray.700" m={4}>
-                        Edit Profile
-                      </Text>
-                      <Avatar
-                        size="4xl"
-                        name="Dani Abrahmov"
-                        mb="20px"
-                        src="https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-                      />
+            <Box
+              w="100%"
+              h="38%"
+              backgroundColor="blue.900"
+              borderTopEndRadius="lg"
+              borderTopStartRadius="lg"
+            />
+            <Wrap
+              w="100%"
+              h="100%"
+              mt="-150px"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              spacing="20px"
+              direction={["column"]}
+            >
+              <WrapItem justifyContent="center">
+                <Avatar
+                  size="4xl"
+                  name="Dani Abrahmov"
+                  src="https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                />
+              </WrapItem>
+              <WrapItem justifyContent="center">
+                <Flex direction="column" alignItems="center">
+                  <Text
+                    fontSize="2xl"
+                    fontWeight="bold"
+                    color="gray.100"
+                    textAlign="center"
+                  >
+                    Dani Abrahmov
+                  </Text>
+                  <Text fontSize="md" color="gray.300" textAlign="center">
+                    Front-end Developer
+                  </Text>
+                </Flex>
+              </WrapItem>
+              <WrapItem w="100%" justifyContent="center" m={2}>
+                <Button colorScheme="facebook" mb={4} onClick={onOpen}>
+                  Edit Profile
+                </Button>
+                <Modal isOpen={isOpen} onClose={onClose}>
+                  <ModalOverlay />
+                  <ModalContent>
+                    <ModalHeader color="gray.700">
+                      <Box
+                        w="100%"
+                        h="100%"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        flexDirection="column"
+                      >
+                        <Text fontSize="4xl" color="gray.700" m={4}>
+                          Edit Profile
+                        </Text>
+                        <Avatar
+                          size="4xl"
+                          name="Dani Abrahmov"
+                          mb="20px"
+                          src="https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                        />
+                        <Button
+                          leftIcon={<DownloadIcon />}
+                          color="gray.500"
+                          _hover={{ bg: "gray.300", color: "gray.700" }}
+                        >
+                          Update image
+                        </Button>
+                      </Box>
+                    </ModalHeader>
+                    <ModalCloseButton color="gray.700" />
+                    <ModalBody>
+                      <FormControl mb={4}>
+                        <Input
+                          placeholder="Name"
+                          color="gray.800"
+                          fontSize="lg"
+                        />
+                      </FormControl>
+                      <FormControl mb={4}>
+                        <Input
+                          placeholder="Position"
+                          color="gray.800"
+                          fontSize="lg"
+                        />
+                      </FormControl>
+                      <FormControl mb={4}>
+                        <Select
+                          placeholder="Departament"
+                          marginBottom={4}
+                          color="gray.700"
+                        >
+                          <option value="option1">Development</option>
+                          <option value="option2">AI</option>
+                          <option value="option3">RPA</option>
+                        </Select>
+                      </FormControl>
+                    </ModalBody>
+                    <ModalFooter>
                       <Button
-                        leftIcon={<DownloadIcon />}
+                        mr={3}
+                        variant="ghost"
                         color="gray.500"
                         _hover={{ bg: "gray.300", color: "gray.700" }}
+                        onClick={onClose}
                       >
-                        Update image
+                        Close
                       </Button>
-                    </Box>
-                  </ModalHeader>
-                  <ModalCloseButton color="gray.700" />
-                  <ModalBody>
-                    <FormControl mb={4}>
-                      <Input
-                        placeholder="Name"
-                        color="gray.800"
-                        fontSize="lg"
-                      />
-                    </FormControl>
-                    <FormControl mb={4}>
-                      <Input
-                        placeholder="Position"
-                        color="gray.800"
-                        fontSize="lg"
-                      />
-                    </FormControl>
-                    <FormControl mb={4}>
-                      <Select
-                        placeholder="Departament"
-                        marginBottom={4}
-                        color="gray.700"
-                      >
-                        <option value="option1">Development</option>
-                        <option value="option2">AI</option>
-                        <option value="option3">RPA</option>
-                      </Select>
-                    </FormControl>
-                  </ModalBody>
-                  <ModalFooter>
-                    <Button
-                      mr={3}
-                      variant="ghost"
-                      color="gray.500"
-                      _hover={{ bg: "gray.300", color: "gray.700" }}
-                      onClick={onClose}
-                    >
-                      Close
-                    </Button>
-                    <Button colorScheme="facebook">Save</Button>
-                  </ModalFooter>
-                </ModalContent>
-              </Modal>
-            </WrapItem>
-          </Wrap>
-        </Box>
+                      <Button colorScheme="facebook">Save</Button>
+                    </ModalFooter>
+                  </ModalContent>
+                </Modal>
+              </WrapItem>
+            </Wrap>
+          </Box>
+        )}
       </Box>
       <Footer />
     </>
--- src/page/Tasks/index.tsx
+++ src/page/Tasks/index.tsx
@@ -13,6 +13,7 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Spinner,
   Table,
   TableCaption,
   TableContainer,
@@ -33,13 +34,15 @@ import { ITasks } from "../../interfaces";
 
 export function Tasks() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isLoading, setIsLoading] = useState(false);
   const [tasks, setTasks] = useState<ITasks[]>([]);
 
   const getAllTasks = async (): Promise<void | Error> => {
+    setIsLoading(true);
     try {
       const response = await api.get("/tasks");
       setTasks(response.data);
-      // console.log(response.data);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -73,105 +76,110 @@ export function Tasks() {
         alignItems="center"
         bgGradient="linear(to-t, gray.900, gray.600)"
       >
-        <Flex direction="column" w="100%" maxW="1120px" mx="auto" px="6">
-          <Button
-            w={["sm", "9rem"]}
-            colorScheme="facebook"
-            size="lg"
-            mt="6"
-            mb="8"
-            leftIcon={<AddIcon />}
-            onClick={onOpen}
-          >
-            <Text fontSize="xl" fontWeight="bold">
-              New Task
-            </Text>
-          </Button>
-          <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-            <ModalContent>
-              <ModalHeader color="gray.700">New Task</ModalHeader>
-              <ModalCloseButton color="gray.700" />
-              <ModalBody>
-                <FormControl mb={4}>
-                  <Input placeholder="Project" color="gray.800" />
-                </FormControl>
-                <FormControl mb={4}>
-                  <Input placeholder="Task" color="gray.800" />
-                </FormControl>
-                <FormControl>
-                  <FormLabel color="gray.700">Estimated time</FormLabel>
-                  <Input
-                    htmlSize={8}
-                    placeholder="start"
-                    width="auto"
-                    mr={8}
-                    color="gray.800"
-                  />
-                  <Input
-                    htmlSize={8}
-                    placeholder="final"
-                    width="auto"
-                    color="gray.800"
-                  />
-                </FormControl>
-              </ModalBody>
-              <ModalFooter>
-                <Button
-                  mr={3}
-                  variant="ghost"
-                  color="gray.700"
-                  _hover={{ bg: "gray.300", color: "gray.700" }}
-                  onClick={onClose}
-                >
-                  Close
-                </Button>
-                <Button colorScheme="facebook">Save</Button>
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
-          <TableContainer bg="gray.800" borderRadius="8" boxShadow="lg" p="4">
-            <Table variant="striped" colorScheme="blackAlpha">
-              <TableCaption placement="top">My Tasks</TableCaption>
-              <Thead>
-                <Tr>
-                  <Th>Project</Th>
-                  <Th>Task</Th>
-                  <Th>Create Date</Th>
-                  <Th>Estimated time</Th>
-                  <Th>Action</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {tasks.map((task) => (
-                  <Tr key={task.user_id}>
-                    <Td>{task.title}</Td>
-                    <Td>{task.description}</Td>
-                    <Td>{date}</Td>
-                    {hourDiff > 1 ? (
-                      <Td>{hourDiff} hours</Td>
-                    ) : (
-                      <Td>{hourDiff} hour</Td>
-                    )}
-                    <Td>
-                      <EditIcon
-                        color="blue.500"
-                        cursor="pointer"
-                        mr="8"
-                        transition="color 0.2s"
-                      />
-                      <DeleteIcon
-                        color="red.500"
-                        cursor="pointer"
-                        transition="all 0.2s"
-                      />
-                    </Td>
+        {isLoading ? (
+          <Spinner size="xl" speed="0.7s" />
+        ) : (
+          <Flex direction="column" w="100%" maxW="1120px" mx="auto" px="6">
+            <Button
+              w={["sm", "9rem"]}
+              colorScheme="facebook"
+              size="lg"
+              mt="6"
+              mb="8"
+              leftIcon={<AddIcon />}
+              onClick={onOpen}
+            >
+              <Text fontSize="xl" fontWeight="bold">
+                New Task
+              </Text>
+            </Button>
+            <Modal isOpen={isOpen} onClose={onClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader color="gray.700">New Task</ModalHeader>
+                <ModalCloseButton color="gray.700" />
+                <ModalBody>
+                  <FormControl mb={4}>
+                    <Input placeholder="Project" color="gray.800" />
+                  </FormControl>
+                  <FormControl mb={4}>
+                    <Input placeholder="Task" color="gray.800" />
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel color="gray.700">Estimated time</FormLabel>
+                    <Input
+                      htmlSize={8}
+                      placeholder="start"
+                      width="auto"
+                      mr={8}
+                      color="gray.800"
+                    />
+                    <Input
+                      htmlSize={8}
+                      placeholder="final"
+                      width="auto"
+                      color="gray.800"
+                    />
+                  </FormControl>
+                </ModalBody>
+                <ModalFooter>
+                  <Button
+                    mr={3}
+                    variant="ghost"
+                    color="gray.700"
+                    _hover={{ bg: "gray.300", color: "gray.700" }}
+                    onClick={onClose}
+                  >
+                    Close
+                  </Button>
+                  <Button colorScheme="facebook">Save</Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+
+            <TableContainer bg="gray.800" borderRadius="8" boxShadow="lg" p="4">
+              <Table variant="striped" colorScheme="blackAlpha">
+                <TableCaption placement="top">My Tasks</TableCaption>
+                <Thead>
+                  <Tr>
+                    <Th>Project</Th>
+                    <Th>Task</Th>
+                    <Th>Create Date</Th>
+                    <Th>Estimated time</Th>
+                    <Th>Action</Th>
                   </Tr>
-                ))}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </Flex>
+                </Thead>
+                <Tbody>
+                  {tasks.map((task) => (
+                    <Tr key={task.user_id}>
+                      <Td>{task.title}</Td>
+                      <Td>{task.description}</Td>
+                      <Td>{date}</Td>
+                      {hourDiff > 1 ? (
+                        <Td>{hourDiff} hours</Td>
+                      ) : (
+                        <Td>{hourDiff} hour</Td>
+                      )}
+                      <Td>
+                        <EditIcon
+                          color="blue.500"
+                          cursor="pointer"
+                          mr="8"
+                          transition="color 0.2s"
+                        />
+                        <DeleteIcon
+                          color="red.500"
+                          cursor="pointer"
+                          transition="all 0.2s"
+                        />
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </Flex>
+        )}
       </Box>
       <Footer />
     </>
